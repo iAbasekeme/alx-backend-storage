acb90423b94e87a1cacf734a7253f81e3be8b962
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-10 01:51:20 +0100

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 --A file that creates a table following specific requirements
 -- id, email, name, country
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
