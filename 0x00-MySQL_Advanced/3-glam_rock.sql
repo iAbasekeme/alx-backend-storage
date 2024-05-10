b85c63d9c26899d5e8bd6b456af997d5c176bd8a
iAbasekeme <abasiekemeetim20@gmail.com>
2024-05-10 01:47:40 +0100

    made file modes change and file automations

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
--- A cript that lists all bands with Glam rock as their
---main style, ranked by their longevity
+-- A cript that lists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
     FROM metal_bands
--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- A script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 DELIMITER $$
 CREATE TRIGGER reset_attr_email
-AFTER UPDATE on users
+BEFORE UPDATE on users
 FOR EACH ROW
 BEGIN
      IF NEW.email != OLD.email
