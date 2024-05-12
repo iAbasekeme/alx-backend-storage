3a50840a37f6c98f0094bedbf5e9b5dd5c9a15c8
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-12 14:49:59 +0100

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,14 +4,11 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
 BEGIN
-    DECLARE result INT; -- Define variable to hold result
-
     IF b = 0 THEN -- Check for division by zero
-        SET result = 0;
+        RETURN 0;
     ELSE
-        SET result = a / b;
+        RETURN a / b;
     END IF;
 
-    RETURN result;
 END $$
 DELIMITER ;
